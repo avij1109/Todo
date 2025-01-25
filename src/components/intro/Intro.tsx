9d8a7a43f93507f14ad435fbcffa2b6a9df11342
avij1109 <avinashjain768@gmail.com>
2025-01-25 19:54:55 +0530

    intro

--- src/components/intro/Intro.tsx
+++ src/components/intro/Intro.tsx
@@ -1,7 +1,14 @@
 import React from "react"
 import "./Intro.css"
-
+import { redirect } from "react-router-dom"
+import {useNavigate} from "react-router-dom"
+import illustrate from '../../assets/illustrate.png'
 const Intro = () => {
+  const navigate = useNavigate();
+  const redirectToSignup = () => {
+    navigate("/signup")
+  }
+
   return (
     <div className="intro-container">
       <div className="intro-content">
@@ -9,10 +16,10 @@ const Intro = () => {
           Welcome to <span className="highlight">TODO</span>
         </h1>
         <p className="intro-subtitle">Organize your life, one task at a time</p>
-        <button className="intro-button">Get Started</button>
+        <button className="intro-button" onClick={redirectToSignup}>Get Started</button>
       </div>
       <div className="intro-image">
-        <img src="/placeholder.svg?height=300&width=300" alt="TODO App Illustration" />
+        <img src={illustrate} alt="TODO App Illustration" className="illus"/>
       </div>
     </div>
   )
